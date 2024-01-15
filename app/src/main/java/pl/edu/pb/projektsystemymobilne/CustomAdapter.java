d94974783a924c1d16f8011930941f25d48e2285
CinekTV <cinekv15@gmail.com>
2024-01-15 23:18:34 +0100

    Działa dodawanie do bazy

---
+++ app/src/main/java/pl/edu/pb/projektsystemymobilne/CustomAdapter.java
@@ -0,0 +1,59 @@
+package pl.edu.pb.projektsystemymobilne;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+    private Context context;
+    private ArrayList anime_main_id, anime_id, anime_name, anime_score;
+
+    CustomAdapter(Context context, ArrayList anime_main_id, ArrayList anime_id, ArrayList anime_name, ArrayList anime_score){
+        this.context = context;
+        this.anime_main_id = anime_main_id;
+        this.anime_id = anime_id;
+        this.anime_name = anime_name;
+        this.anime_score = anime_score;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.anime_main_id_txt.setText(String.valueOf(anime_main_id.get(position)));
+        holder.anime_id_txt.setText(String.valueOf(anime_id.get(position)));
+        holder.anime_name_txt.setText(String.valueOf(anime_name.get(position)));
+        holder.anime_score_txt.setText(String.valueOf(anime_score.get(position)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return anime_main_id.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView anime_main_id_txt, anime_id_txt, anime_name_txt, anime_score_txt;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            anime_main_id_txt = itemView.findViewById(R.id.anime_main_id_txt);
+            anime_id_txt = itemView.findViewById(R.id.anime_id_txt);
+            anime_name_txt = itemView.findViewById(R.id.anime_name_txt);
+            anime_score_txt = itemView.findViewById(R.id.anime_score_txt);
+        }
+    }
+}
--- app/src/main/java/pl/edu/pb/projektsystemymobilne/UserProfile.java
+++ app/src/main/java/pl/edu/pb/projektsystemymobilne/UserProfile.java
@@ -1,18 +1,28 @@
 package pl.edu.pb.projektsystemymobilne;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class UserProfile extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FloatingActionButton add_button;
+
+    MyDatabaseHelper myDB;
+    ArrayList<String> main_id, anime_id, anime_name, anime_score;
+    CustomAdapter customAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,5 +37,31 @@ public class UserProfile extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        myDB = new MyDatabaseHelper(UserProfile.this);
+        main_id = new ArrayList<>();
+        anime_id = new ArrayList<>();
+        anime_name = new ArrayList<>();
+        anime_score = new ArrayList<>();
+
+        storeDataInArrays();
+
+        customAdapter = new CustomAdapter(UserProfile.this, main_id, anime_id, anime_name, anime_score);
+        recyclerView.setAdapter(customAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager( UserProfile.this));
+    }
+
+    void storeDataInArrays(){
+        Cursor cursor = myDB.readAllData();
+        if (cursor.getCount() == 0){
+            Toast.makeText(this, "No data.", Toast.LENGTH_SHORT).show();
+        }else {
+            while (cursor.moveToNext()){
+                main_id.add(cursor.getString(0));
+                anime_name.add(cursor.getString(1));
+                anime_id.add(cursor.getString(2));
+                anime_score.add(cursor.getString(7));
+            }
+        }
     }
 }
